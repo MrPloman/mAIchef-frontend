a9eb22cb020fbdab5e16514b5f2325d4f6e1b8d3
MrPloman <polplanaayet@gmail.com>
2026-03-21 09:30:30 -0500

    created component apart

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoaderComponent } from '../../shared/ui/loader/loader.component/loader.component';
+import { RecipeGeneratorComponent } from '../../shared/ui/recipe-generator/recipe-generator.component';
 
 @Component({
   selector: 'app-home',
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, RecipeGeneratorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/app/shared/ui/recipe-generator/recipe-generator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-generator',
+  imports: [],
+  templateUrl: './recipe-generator.component.html',
+  styleUrl: './recipe-generator.component.scss',
+})
+export class RecipeGeneratorComponent {}
