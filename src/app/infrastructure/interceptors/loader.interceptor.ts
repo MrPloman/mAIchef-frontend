bb31ef956ac52d672db809ba447816aeabe40e55
MrPloman <polplanaayet@gmail.com>
2026-03-22 15:29:16 -0500

    trying to update dependencies

---
+++ src/app/infrastructure/interceptors/loader.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { LoaderFacade } from '../../store/facades/loader.facade';
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+  constructor(private readonly loaderFacade: LoaderFacade) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    if (request.headers.get('X-Skip-Loader') === 'true') {
+      return next.handle(request);
+    }
+
+    this.loaderFacade.show();
+
+    return next.handle(request).pipe(finalize(() => this.loaderFacade.hide()));
+  }
+}
