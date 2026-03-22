c75e52472c3ada0f48313db71d1eeac37cfe462b
MrPloman <polplanaayet@gmail.com>
2026-03-22 09:59:25 -0500

    loader movement in messages

--- src/app/shared/ui/loader/loader.component/loader.component.ts
+++ src/app/shared/ui/loader/loader.component/loader.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -6,5 +11,40 @@ import { Component } from '@angular/core';
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoaderComponent {}
+export class LoaderComponent {
+  public messages: string[] = [
+    'We are working on it...',
+    'This is gonna be delicious...',
+    'Just a few more seconds...',
+    'We are getting it.',
+  ];
+  public currentMessagePosition: number = 0;
+  public currentMessage: WritableSignal<string> = signal(
+    this.messages[this.currentMessagePosition],
+  );
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    setInterval(() => {
+      this.nextMessage();
+    }, 4000);
+  }
+  public nextMessage() {
+    const message = document.getElementById('textStatus');
+    message?.classList.add('slide-middle-up-element');
+    setTimeout(() => {
+      if (this.currentMessagePosition < this.messages.length - 1) {
+        this.currentMessagePosition++;
+        this.currentMessage.set(this.messages[this.currentMessagePosition]);
+      } else {
+        this.currentMessagePosition = 0;
+        this.currentMessage.set(this.messages[this.currentMessagePosition]);
+      }
+    }, 500);
+
+    console.log(this.currentMessage);
+  }
+}
