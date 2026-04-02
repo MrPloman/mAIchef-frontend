655ca0d3e9c7aa76f1cb0f625e627d55f516284d
MrPloman <polplanaayet@gmail.com>
2026-04-02 15:33:16 -0500

    tsconfig modified

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
