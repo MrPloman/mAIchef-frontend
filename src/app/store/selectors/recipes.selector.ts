958d547b053a98b31709fb40719d682bf19a3e4a
MrPloman <polplanaayet@gmail.com>
2026-04-14 19:41:13 -0500

    selector for store created

---
+++ src/app/store/selectors/recipes.selector.ts
@@ -0,0 +1,16 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { RecipesState } from '../state/recipes.state';
+
+export const RECIPES_FEATURE_KEY = 'recipes';
+
+export const selectRecipesState =
+  createFeatureSelector<RecipesState>(RECIPES_FEATURE_KEY);
+
+export const selectRequestedRecipes = createSelector(
+  selectRecipesState,
+  (recipesState) => recipesState.requestedRecipes,
+);
+export const selectSelectedRecipe = createSelector(
+  selectRecipesState,
+  (recipesState) => recipesState.selectedRecipe,
+);
--- src/app/store/state/recipes.state.ts
+++ src/app/store/state/recipes.state.ts
@@ -1,6 +1,6 @@
 import { Recipe } from '../../core/domain/models/recipe/recipe.model';
 
-export interface RequestedRecipesState {
+export interface RecipesState {
   requestedRecipes: Recipe[];
   selectedRecipe: number | null;
 }
