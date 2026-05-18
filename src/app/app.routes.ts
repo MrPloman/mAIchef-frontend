087318e5b729ec7c302a796b630ff925ed935952
MrPloman <polplanaayet@gmail.com>
2026-05-18 09:14:24 -0500

    second guard activate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,24 @@ import { AuthComponent } from './features/auth/auth.component';
 import { HomeComponent } from './features/home/home.component';
 import { RecipeDetailComponent } from './features/recipe-detail/recipe-detail.component';
 import { ResultsComponent } from './features/results/results.component';
-import { recipeGuard } from './shared/guards/recipe.guard';
+import {
+  recipeDetailGuard,
+  recipesRequestedGuard,
+} from './shared/guards/recipe.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'results', component: ResultsComponent },
+  {
+    path: 'results',
+    component: ResultsComponent,
+    canActivate: [recipesRequestedGuard],
+  },
   { path: 'profile', component: AuthComponent },
   {
     path: 'recipe/:id',
     component: RecipeDetailComponent,
-    canActivate: [recipeGuard],
-  }, // Add auth guard here
+    canActivate: [recipeDetailGuard],
+  },
   { path: '**', redirectTo: '/home' }, // Wildcard must be last
 ];
--- src/app/shared/guards/recipe.guard.ts
+++ src/app/shared/guards/recipe.guard.ts
@@ -5,17 +5,32 @@ import { Recipe } from '../../core/domain/models/recipe/recipe.model';
 import { RecipesFacade } from '../../store/facades/recipes.facade';
 import { RecipesHelper } from '../helpers/recipes.helper';
 
-export const recipeGuard: CanActivateFn = (route, state) => {
-  const recipeFacade = inject(RecipesFacade).returnRecipesRequested;
+export const recipeDetailGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const recipeFacade = inject(RecipesFacade);
   const recipeHelper = inject(RecipesHelper);
+
   const _id = route.params['id'];
   let recipes: Recipe[] = [];
-  recipeFacade.subscribe((recipeList) => {
+  recipeFacade.returnRecipesRequested.subscribe((recipeList) => {
     recipes = recipeList;
   });
 
   return recipeHelper.getRecipeById(_id, recipes)
     ? true
-    : router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    : router.navigate(['/home']);
+};
+
+export const recipesRequestedGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const recipeFacade = inject(RecipesFacade);
+  let recipes: Recipe[] = [];
+  recipeFacade.returnRecipesRequested.subscribe((recipeList) => {
+    recipes = recipeList;
+  });
+  if (recipes.length > 0) {
+    return true;
+  } else {
+    return router.navigate(['/home']);
+  }
 };
